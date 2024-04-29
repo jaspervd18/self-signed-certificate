f111710aa43c5ce144521e75c9f2651ac640d890
Jasper Van Dyck <vandyckja@delawareconsulting.com>
2024-04-29 19:06:34 +0200

    tweaking

--- openssl_script.sh
+++ openssl_script.sh
@@ -10,10 +10,10 @@ openssl genrsa -out $SSL_FOLDER/rootCA.key 2048
 # Set the validity period to 365 days and use SHA256 for the signature algorithm
 openssl req -x509 -new -nodes -key $SSL_FOLDER/rootCA.key -sha256 -days 365 -out $SSL_FOLDER/rootCA.crt -config openssl.conf
 
-# Generate a new private key (mydomain.com.key) with a length of 2048 bits
+# Generate a new private key (mydomain.key) with a length of 2048 bits
 openssl genrsa -out $SSL_FOLDER/mydomain.key 2048
 
-# Create a new certificate signing request (mydomain.com.csr) using the private key (mydomain.com.key)
+# Create a new certificate signing request (mydomain.csr) using the private key (mydomain.key)
 openssl req -new -key $SSL_FOLDER/mydomain.key -out $SSL_FOLDER/mydomain.csr -config openssl.conf
 
 # Sign the CSR using the root CA certificate (rootCA.crt) and private key (rootCA.key)
@@ -29,7 +29,13 @@ openssl x509 \
   -extensions req_ext \
   -extfile openssl.conf
 
-openssl pkcs12 -export -out $SSL_FOLDER/mydomain.pfx -inkey $SSL_FOLDER/mydomain.key -in $SSL_FOLDER/mydomain.crt -certfile $SSL_FOLDER/rootCA.crt -passin pass:password -passout pass:password
+openssl pkcs12 \
+  -export -out $SSL_FOLDER/mydomain.pfx \
+  -inkey $SSL_FOLDER/mydomain.key \
+  -in $SSL_FOLDER/mydomain.crt \
+  -certfile $SSL_FOLDER/rootCA.crt \
+  -passin pass:password \
+  -passout pass:password
 
 # View the contents of the signed certificate to verify the information
 # openssl x509 -in mydomain.com.crt -text -noout
